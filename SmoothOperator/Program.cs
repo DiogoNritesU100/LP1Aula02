bd5340fd2f7f95376de5618f9c9930f2c9e3bff5
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-09 19:17:26 +0000

    SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 1;
+        int b = 5;
+        int c = a++ + --b;
+        
+        Console.WriteLine($"a={a}"); // 2
+        Console.WriteLine($"b={b}"); // 4
+        Console.WriteLine($"c={c}"); // 1 + 4 = 5
+        
+
+    }
+}
