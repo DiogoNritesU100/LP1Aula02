b347a80d58b902d9f4aca87cc992979eed6f86d9
DiogoNritesU100 <qbrites0@gmail.com>
2025-03-13 16:17:26 +0000

    Solve exercise 6: Valores Especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"int max = {int.MaxValue}");
+        Console.WriteLine($"int min = {int.MinValue}");
+        Console.WriteLine($"double max = {double.MaxValue}");
+        Console.WriteLine($"double min = {double.MinValue}");
+        Console.WriteLine($"byte max = {byte.MaxValue}");
+        Console.WriteLine($"byte min = {byte.MinValue}");
+
+        Console.WriteLine($"double +inf = {double.PositiveInfinity}");
+        Console.WriteLine($"double -inf = {double.NegativeInfinity}");
+        Console.WriteLine($"double Nan = {double.NaN}");
+
+        
+    }
+}
